712222762b7093d5b8f7bb2a07cd6fc64ec44d2b
mustafaevadd-coder <mustafaeva.dd@gmail.com>
2026-02-15 20:45:45 +0300

    Add files via upload

---
+++ ABC_lab1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int number;
+	int k;
+
+	cout << "Введите число: ";
+	cin >> number;
+
+	cout << "Введите количество сдвигов:";
+	cin >> k;
+
+	int leftt = number << k;
+	int rightt = number >> k;
+
+	cout << "Результат сдвига влево: " << leftt << endl;
+	cout << "Результат сдвига вправо: " << rightt << endl;
+
+	return 0;
+
+}
